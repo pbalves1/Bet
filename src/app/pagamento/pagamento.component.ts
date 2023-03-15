80aa6ed0a98117308b115461654c8a2f86d0c9c9
Wilquer <wilquer.fig@gmail.com>
2023-03-15 17:20:15 -0300

    cores front

--- src/app/pagamento/pagamento.component.ts
+++ src/app/pagamento/pagamento.component.ts
@@ -18,14 +18,20 @@ export class PagamentoComponent implements OnInit {
 
   errorMessage:string;
 
-  callMeli(){
-    window.location.href = "https://www.mercadopago.com.br/subscriptions/checkout?preapproval_plan_id=2c938084869290d0018694c9c4cf01a7";  
+  callMeli(number) {
+    if (number == 1) {
+      window.location.href = "https://www.mercadopago.com.br/subscriptions/checkout?preapproval_plan_id=2c93808486ded3a10186e1c5c87e0158";
+    }
+    if (number == 3){
+      window.location.href ="https://www.mercadopago.com.br/subscriptions/checkout?preapproval_plan_id=2c93808486dab2050186e1c99cd40433"
+    }
+    if (number == 6){
+      window.location.href ="https://www.mercadopago.com.br/subscriptions/checkout?preapproval_plan_id=2c93808486dfe74f0186e1cbcc940133"
+    }
+
   }
-  
 
   ngOnInit(): void {
-    
-  }
 
- 
+  }
 }
